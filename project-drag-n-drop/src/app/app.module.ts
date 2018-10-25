780b49913e1e81113f36a226ac3cc533c89f5327
Randall Crame <rcrame05@gmail.com>
2018-10-24 23:45:33 -0400

    Corrections and inserting Sidebar imports

--- project-drag-n-drop/src/app/app.module.ts
+++ project-drag-n-drop/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatExpansionModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import { SkyhookDndModule } from "@angular-skyhook/core";
+import { default as HTML5Backend } from 'react-dnd-html5-backend';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    BrowserAnimationsModule,
+    MatExpansionModule,
+    SkyhookDndModule.forRoot({ backend: HTML5Backend })
   ],
   providers: [],
   bootstrap: [AppComponent]
