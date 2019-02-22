cbde4f4b8c4b6952d4fcdecb016911c1314e2901
Randall Crame <rcrame05@gmail.com>
2019-02-21 23:59:52 -0500

    Testing boundary dnd

--- project-drag-n-drop/src/app/app.component.ts
+++ project-drag-n-drop/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project-drag-n-drop';
+  race = [
+    'Human',
+    'Elf',
+    'Dwarf',
+  ];
+
+  selectedRace = ['Empty',
+                  'List'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
 }
--- project-drag-n-drop/src/app/app.module.ts
+++ project-drag-n-drop/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatExpansionModule } from '@angular/material';
+import {MatGridListModule} from '@angular/material/grid-list';
 import { AppComponent } from './app.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { SkyhookDndModule } from "@angular-skyhook/core";
@@ -16,6 +17,7 @@ import { default as HTML5Backend } from 'react-dnd-html5-backend';
   imports: [
     BrowserModule, 
     BrowserAnimationsModule,
+    MatGridListModule,
     DragDropModule,
     MatExpansionModule,
     SkyhookDndModule.forRoot({ backend: HTML5Backend })
